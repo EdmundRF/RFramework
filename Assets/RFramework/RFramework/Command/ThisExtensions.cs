4a252345791a13a135f564c51df640711e39c238
EdmundRF <423775511@qq.com>
2021-11-02 10:56:24 +0800

    This拓展List

---
+++ Assets/RFramework/RFramework/Command/ThisExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RFramework
+{
+    public static class ThisExtensions
+    {
+        /// <summary>
+        /// 移除所有null元素
+        /// </summary>
+        /// <param name="ls"></param>
+        public static void ClearNull(this List<GameObject> ls)
+        {
+            List<GameObject> del = new List<GameObject>();
+            for (int i = 0; i < ls.Count; i++)
+            {
+                if (ls[i] == null)
+                {
+                    del.Add(ls[i]);
+                }
+            }
+
+            foreach (var i in del)
+            {
+                ls.Remove(i);
+            }
+        }
+    }
+}
